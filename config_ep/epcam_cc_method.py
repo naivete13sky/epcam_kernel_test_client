a5ab6d99b8db724480b896330fd0f2a1b17d5f16
naivete13sky <chen320821@163.com>
2022-10-31 15:36:31 +0800

    20221031 2

---
+++ config_ep/epcam_cc_method.py
@@ -0,0 +1,16 @@
+from epcam_api import Configuration, Input, GUI
+
+Configuration.init(r'C:\cc\ep_local\product\EP-CAM\version\20221031\EP-CAM_beta_2.29.055_s19_jiami\Release')
+Configuration.set_sys_attr_path(r'C:\cc\ep_local\product\EP-CAM\version\20221031\EP-CAM_beta_2.29.055_s19_jiami\Release\config\attr_def')
+Configuration.set_user_attr_path(r'C:\cc\ep_local\product\EP-CAM\version\20221031\EP-CAM_beta_2.29.055_s19_jiami\Release\config\attr_def')
+
+
+def f1():
+    pass
+    Input.open_job("eni40021", r"C:\job\test\odb")
+    GUI.show_layer("eni40021", "orig", "top")
+
+
+if __name__ == "__main__":
+    pass
+    f1()
