4c9421d9f4b07084f614ade897b9834d751cfd37
naivete13sky <chen320821@163.com>
2022-10-31 15:57:16 +0800

    20221031 4 设置属性

--- config_ep/epcam_cc_method.py
+++ config_ep/epcam_cc_method.py
@@ -1,8 +1,8 @@
 from epcam_api import Configuration, Input, GUI
 
 Configuration.init(r'C:\cc\ep_local\product\EP-CAM\version\20221031\EP-CAM_beta_2.29.055_s19_jiami\Release')
-Configuration.set_sys_attr_path(r'C:\cc\ep_local\product\EP-CAM\version\20221031\EP-CAM_beta_2.29.055_s19_jiami\Release\config\attr_def')
-Configuration.set_user_attr_path(r'C:\cc\ep_local\product\EP-CAM\version\20221031\EP-CAM_beta_2.29.055_s19_jiami\Release\config\attr_def')
+Configuration.set_sys_attr_path(r'C:\cc\ep_local\product\EP-CAM\version\20221031\EP-CAM_beta_2.29.055_s19_jiami\Release\config\attr_def\sysattr')
+Configuration.set_user_attr_path(r'C:\cc\ep_local\product\EP-CAM\version\20221031\EP-CAM_beta_2.29.055_s19_jiami\Release\config\attr_def\userattr')
 
 
 def f1():
