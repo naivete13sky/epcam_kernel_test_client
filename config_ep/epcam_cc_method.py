e6e35f9a8d67c553c13a46335de8cca3ef5c4e11
naivete13sky <chen320821@163.com>
2022-11-08 10:53:00 +0800

    20221108 3

--- config_ep/epcam_cc_method.py
+++ config_ep/epcam_cc_method.py
@@ -19,9 +19,13 @@ def f1():
     GUI.show_layer("eni40021", "orig", "top")
 
 class MyInput(object):
-
-    def input_gerber_folder(self,folder_path,job,step,save_path):
+    def input_gerber_folder(self,folder_path,job,step,*,save_path=None):
+        '''
+        命名关键字参数save_path，用来保存料号的路径
+        '''
         #创建一个空料号
+        if not save_path:
+            save_path = r'C:\job\test\odb'
         new_job_path = os.path.join(save_path, job)  # job若存在则删除
         if os.path.exists(new_job_path):
             shutil.rmtree(new_job_path)
@@ -36,7 +40,9 @@ class MyInput(object):
             print(result_each_file_identify)
             Input.file_translate(path=os.path.join(folder_path,each_file),job=job, step='orig', layer=each_file, param=result_each_file_identify)
 
-        BASE.save_job_as(job,r'C:\job\test\odb')
+
+        BASE.save_job_as(job, save_path)
+
 
         GUI.show_layer(job, "orig", "layer")
 
@@ -80,12 +86,10 @@ class MyJob(object):
 
 
 if __name__ == "__main__":
-
-    job_save_path_parent = r'C:\job\test\odb'
     folder_path = r"C:\Users\cheng.chen\Desktop\760"
     job = r'test'
     step = r'orig'
     save_path = r'C:\job\test\odb'
     cc = MyInput()
-    cc.input_gerber_folder(folder_path,job,step,save_path)
+    cc.input_gerber_folder(folder_path,job,step)
 
