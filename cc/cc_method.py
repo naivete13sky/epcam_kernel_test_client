de4cac2b9145dcdd2be098023198482198a48a91
naivete13sky <chen320821@163.com>
2022-11-08 10:26:48 +0800

    20221108 2

--- cc/cc_method.py
+++ cc/cc_method.py
@@ -240,7 +240,18 @@ def getFlist(path):
         print('files:', files)     #文件名称，返回list类型
     return files
 
-
+class StringMehtod(object):
+    @staticmethod
+    def is_chinese(string):
+        """判断是否有中文
+        :param     string(str):所有字符串
+        :returns   :False
+        :raises    error:
+        """
+        for ch in string:
+            if u'\u4e00' <= ch <= u'\u9fff':
+                return True
+        return False
 
 #存储为json文件的方法
 def else1():
--- config_ep/epcam_cc_method.py
+++ config_ep/epcam_cc_method.py
@@ -20,17 +20,40 @@ def f1():
 
 class MyInput(object):
 
-    @staticmethod
-    def is_chinese(string):
-        """判断是否有中文
-        :param     string(str):所有字符串
-        :returns   :False
-        :raises    error:
-        """
-        for ch in string:
-            if u'\u4e00' <= ch <= u'\u9fff':
-                return True
-        return False
+    def input_gerber_folder(self,folder_path,job,step,save_path):
+        #创建一个空料号
+        new_job_path = os.path.join(save_path, job)  # job若存在则删除
+        if os.path.exists(new_job_path):
+            shutil.rmtree(new_job_path)
+        Job.create_job(job)
+        #创建一个空step
+        Matrix.create_step(job, step)
+        #开始识别文件夹中各个文件的类型，此方只识别单层文件夹中的内容
+        file_list = [x for x in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path,x))]
+        print('file_list:',file_list)
+        for each_file in file_list:
+            result_each_file_identify = Input.file_identify(os.path.join(folder_path,each_file))
+            print(result_each_file_identify)
+            Input.file_translate(path=os.path.join(folder_path,each_file),job=job, step='orig', layer=each_file, param=result_each_file_identify)
+
+        BASE.save_job_as(job,r'C:\job\test\odb')
+
+        GUI.show_layer(job, "orig", "layer")
+
+    def file_identify(self):
+        pass
+
+    def input_gerber_one_file(self,job):
+        Job.create_job(job)
+        Matrix.create_step(job, 'orig')
+        result_file_identify = Input.file_identify(r"C:\Users\cheng.chen\Desktop\760\LAYER2.art")
+        # print(result_file_identify)
+        Input.file_translate(path = r"C:\Users\cheng.chen\Desktop\760\LAYER2.art",
+                             job = job, step = 'orig', layer = 'layer',param = result_file_identify)
+
+        BASE.save_job_as(job,r'C:\job\test\odb')
+
+        GUI.show_layer(job, "orig", "layer")
 
     def input_gerber(self,job):
         Job.create_job(job)
@@ -43,10 +66,6 @@ class MyInput(object):
         BASE.save_job_as(job,r'C:\job\test\odb')
 
         GUI.show_layer(job, "orig", "layer")
-
-
-    def file_identify(self):
-        pass
 
 class MyJob(object):
     def my_save_job_odb(self,job):
@@ -61,9 +80,12 @@ class MyJob(object):
 
 
 if __name__ == "__main__":
-    pass
+
     job_save_path_parent = r'C:\job\test\odb'
+    folder_path = r"C:\Users\cheng.chen\Desktop\760"
     job = r'test'
+    step = r'orig'
+    save_path = r'C:\job\test\odb'
     cc = MyInput()
-    cc.input_gerber(job)
+    cc.input_gerber_folder(folder_path,job,step,save_path)
 
