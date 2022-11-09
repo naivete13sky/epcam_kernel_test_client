bcaba1d27f5b917c8ba37deadfa52b385ccf0961
naivete13sky <chen320821@163.com>
2022-11-09 10:49:42 +0800

    20221109 1

--- test_dir/test_input.py
+++ test_dir/test_input.py
@@ -32,9 +32,41 @@ class TestInputGerber274X:
         temp_ep_path = os.path.join(temp_path, 'ep')
         temp_g_path = os.path.join(temp_path, 'g')
 
+        # 下载并解压原始gerber文件
+        DMS().get_file_from_dms_db(temp_path, job_id, field='file_compressed', decompress='rar')
+        # 悦谱转图
+        folder_path = os.path.join(temp_gerber_path,os.listdir(temp_gerber_path)[0].lower())
+        job_ep = os.listdir(temp_gerber_path)[0].lower()  + '_ep'
+        step = r'orig'
+        save_path = temp_ep_path
+        my_input = MyInput(folder_path, job_ep, step,save_path=save_path)
+        my_input.fix_layer_name_same_to_g()
+        my_input.input_folder()
+
+
         # 下载G转图tgz，并解压好
         DMS().get_file_from_dms_db(temp_path, job_id, field='file_odb_g', decompress='tgz')
         job = os.listdir(temp_g_path)[0]
+
+        # 打开job_ep
+        print("job_ep:", job_ep)
+        all_layers_list_job_ep = Information.get_layers(job_ep)
+        if len(all_layers_list_job_ep) == 0:
+            g_vs_total_result_flag = False
+            print("最新-EP-ODB++打开失败！！！！！")
+        else:
+            print('悦谱软件tgz中的层信息：', all_layers_list_job_ep)
+
+        # 打开job_g
+        job_g = os.listdir(temp_g_path)[0]
+        Input.open_job(job_g, temp_g_path)
+        all_layers_list_job_g = Information.get_layers(job_g)
+        if len(all_layers_list_job_g) == 0:
+            g_vs_total_result_flag = False
+            print("G-ODB++打开失败！！！！！")
+        else:
+            print('G软件tgz中的层信息：', all_layers_list_job_g)
+
 
         assert job_id == 511
 
